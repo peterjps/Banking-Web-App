7a79fddeae9ce168cf789d9a73683f6a082d544a
Peter <pedropseverino@gmail.com>
2024-06-27 16:41:40 -0400

    Push to Github

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
@@ -8,6 +9,7 @@ export async function createSessionClient() {
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
   const session = cookies().get("appwrite-session");
+
   if (!session || !session.value) {
     throw new Error("No session");
   }
@@ -22,28 +24,20 @@ export async function createSessionClient() {
 }
 
 export async function createAdminClient() {
-    
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
     .setKey(process.env.NEXT_APPWRITE_KEY!);
 
-//   const session = cookies().get("appwrite-session");
-//   if (!session || !session.value){
-//     throw new Error("No session");
-//   }
-
-//   client.setSession(session.value);
-
-    return {
+  return {
     get account() {
       return new Account(client);
     },
     get database() {
-        return new Databases(client);
+      return new Databases(client);
     },
     get user() {
-        return new Users(client);
+      return new Users(client);
     }
   };
 }
